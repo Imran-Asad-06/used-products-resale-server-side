c4e36d833aad4983de7423a0f4f92054f135f828
Imran-Asad-06 <imran.asad.0607@gmail.com>
2022-11-28 17:37:34 +0600

    added user part

--- index.js
+++ index.js
@@ -118,7 +118,55 @@ async function run(){
               res.status(403).send({ accessToken: '' })
           });
       
-          
+          app.post('/users', async (req, res) => {
+              const user = req.body;
+              console.log(user);
+              const result = await usersCollection.insertOne(user);
+              res.send(result);
+          });
+  
+          app.put('/users/:id', async (req, res) => {
+              const id = req.params.id;
+              const filter = { _id: ObjectId(id) }
+              const options = { upsert: true };
+              const updatedDoc = {
+                  $set: {
+                     role: 'admin'
+                      
+                  }
+              }
+              const result = await usersCollection.updateOne(filter, updatedDoc, options);
+              res.send(result);
+          });
+  
+          app.get('/users/:email', async (req, res) => {
+              const email = req.params.email;
+              const query = { email }
+              const user = await usersCollection.findOne(query);
+              res.send({ isAdmin: user?.role === 'admin'},
+  
+              );
+          })
+          app.get('/users/:email', async (req, res) => {
+              const email = req.params.email;
+              const query = { email }
+              const user = await usersCollection.findOne(query);
+              res.send(
+              {isBuyer: user?.role  === 'buyer'},
+              );
+          })
+          app.get('/users/:email', async (req, res) => {
+              const email = req.params.email;
+              const query = { email }
+              const user = await usersCollection.findOne(query);
+              res.send(
+              
+              {isSeller: user?.role === 'seller'});
+          })
+  
+         
+  
+         
           
   
     }
