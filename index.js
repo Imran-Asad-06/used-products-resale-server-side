fc5146685714baad3376aae0b5f2481a74092b85
Imran-Asad-06 <imran.asad.0607@gmail.com>
2022-11-28 16:03:49 +0600

    verify admin seller buyer

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use(cors());
 app.use(express.json());
 
 //user and password
-const uri = "mongodb+srv://DB_USER:DB_PASSWORD@cluster0.stfbiry.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.stfbiry.mongodb.net/?retryWrites=true&w=majority`;
+
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 //jwt 
@@ -38,3 +40,59 @@ function verifyJWT(req, res, next) {
 
 }
 
+async function run(){
+    try{
+      const categoriesCollection = client.db('old-furniture').collection('categories');
+      const productCollection = client.db('old-furniture').collection('resaleProducts');
+      const usersCollection = client.db('old-furniture').collection('allUsers');
+      const bookingsCollection = client.db('old-furniture').collection('booking');
+      
+  
+      const verifyAdmin = async (req, res, next) => {
+          const decodedEmail = req.decoded.email;
+          const query = { email: decodedEmail };
+          const user = await usersCollection.findOne(query);
+  
+          if (user?.role !== 'admin') {
+              return res.status(403).send({ message: 'forbidden access' })
+          }
+          next();
+      }
+      const verifyBuyer = async (req, res, next) => {
+          const decodedEmail = req.decoded.email;
+          const query = { email: decodedEmail };
+          const user = await usersCollection.findOne(query);
+  
+          if (user?.role !== 'buyer') {
+              return res.status(403).send({ message: 'forbidden access' })
+          }
+          next();
+      }
+      const verifySeller = async (req, res, next) => {
+          const decodedEmail = req.decoded.email;
+          const query = { email: decodedEmail };
+          const user = await usersCollection.findOne(query);
+  
+          if (user?.role !== 'seller') {
+              return res.status(403).send({ message: 'forbidden access' })
+          }
+          next();
+      }
+  
+      
+  
+    }
+    finally{
+  
+    }
+  }
+  run().catch(e=> console.log(e))
+  
+  
+  app.get('/', async (req, res) => {
+      res.send('old furniture server is running');
+  })
+  
+  app.listen(port, () => {console.log(`old furniture Bin running on ${port}`);
+  })
+  
